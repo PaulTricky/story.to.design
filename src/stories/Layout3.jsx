6c27495abd1422663d28bb5c16a20c4ffb044600
Paul <>
2023-11-28 21:25:25 +0700

    u

---
+++ src/stories/Layout3.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './page.css';
+import LayoutPage from '../components/#3';
+import { ConfigProvider } from 'antd';
+import { theme } from '../../theme.config';
+
+export const Layout3Story = () => {
+  return (
+    <ConfigProvider theme={theme}>
+      <LayoutPage />
+    </ConfigProvider>
+  );
+};
