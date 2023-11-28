80a5a67726b62e9675d8684b8d838e98aa728cfe
Paul <>
2023-11-28 19:31:19 +0700

    u

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/globals.scss';
 import '../styles/styles.scss';
 import { ConfigProvider } from 'antd';
 import { theme } from '../../theme.config';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }) {
   return (
--- src/stories/Login.jsx
+++ src/stories/Login.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 
 import './page.css';
 import LoginPage from '../domains/Login';
+import { ConfigProvider } from 'antd';
+import { theme } from '../../theme.config';
 
 export const LoginStory = () => {
-  return <LoginPage />;
+  return (
+    <ConfigProvider theme={theme}>
+      <LoginPage />
+    </ConfigProvider>
+  );
 };
